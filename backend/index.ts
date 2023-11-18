609fda4e839b6c776f11399d42b72324ba02abec
Choi kyun ho <rbsgh777@naver.com>
2023-11-18 11:57:33 +0900

    Writing

--- backend/index.ts
+++ backend/index.ts
@@ -2,6 +2,7 @@ import express, { Express } from "express";
 import cors from "cors";
 import userRouter from "./routes/user";
 import authRouter from "./routes/auth";
+import postRouter from "./routes/post";
 
 const app: Express = express();
 const port: number = +process.env.PORT! | 3010;
@@ -17,6 +18,7 @@ app.use(
 
 app.use("/user", userRouter);
 app.use("/auth", authRouter);
+app.use("/post", postRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello, ExpressTS!");
